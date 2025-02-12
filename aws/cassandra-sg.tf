f0bf0ea5366d2a748f09da1a0fc83c59b7af88b9
Tomasz Hermanowicz <hermangti@gmail.com>
2025-02-12 18:09:44 +0100

    added rules in sg for base cassandra

---
+++ aws/cassandra-sg.tf
@@ -0,0 +1,64 @@
+resource "aws_security_group" "cassandra-sg" {
+  vpc_id      = module.vpc.vpc_id
+  name        = "cassandra-sg"
+  description = "primary cassandra security group with default rules"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    description = "sshd"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  ingress {
+    from_port   = 7000
+    to_port     = 7000
+    protocol    = "tcp"
+    description = "Inter cluster comms"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  ingress {
+    from_port   = 7199
+    to_port     = 7199
+    protocol    = "tcp"
+    description = "jmx cluster monitoring"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  ingress {
+    from_port   = 7001
+    to_port     = 7001
+    protocol    = "tcp"
+    description = "inter cluster comms wit ssl"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  ingress {
+    from_port   = 9042
+    to_port     = 9042
+    protocol    = "tcp"
+    description = "cassandra native client port"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  ingress {
+    from_port   = 9142
+    to_port     = 9142
+    protocol    = "tcp"
+    description = "cassandra native client port with ssl"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Terraform   = true
+    Environment = "sandbox"
+    Version     = "0.1.0"
+  }
+}
--- aws/vpc.tf
+++ aws/vpc.tf
@@ -12,12 +12,13 @@ module "vpc" {
   intra_subnets        = ["10.0.33.0/24", "10.0.36.0/24", "10.0.39.0/24"]
   intra_subnet_names   = ["cache-a", "cache-b", "cache-c"]
   public_subnets       = ["10.0.203.0/24", "10.0.206.0/24", "10.0.209.0/24"]
+  public_subnet_names  = ["lbs-${var.region}a", "lbs-${var.region}b", "lbs-${var.region}c"]
 
   enable_nat_gateway     = true
   single_nat_gateway     = true
   one_nat_gateway_per_az = false
 
-  database_subnet_group_name         = "primary_db_subnet_group"
+  database_subnet_group_name         = "primary-db-subnet-group"
   create_database_subnet_group       = true
   create_database_subnet_route_table = true
 
